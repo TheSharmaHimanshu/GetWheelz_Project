73b8df7d4b85386b29143e9185c54d1af30d0e85
TheSharmaHimanshu <thesharmahimanshu@gmail.com>
2019-12-06 17:04:32 +0100

    Initial Commit with NavBar and Search

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule, MatToolbarModule, MatIconModule, MatInputModule } from '@angular/material';
+
+
+const MaterialComponents = [
+  MatButtonModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatInputModule
+];
+
+@NgModule({
+  imports: [MaterialComponents, MatToolbarModule, MatIconModule, MatInputModule],
+  exports: [MaterialComponents, MatToolbarModule, MatIconModule, MatInputModule]
+})
+export class MaterialModule { }
