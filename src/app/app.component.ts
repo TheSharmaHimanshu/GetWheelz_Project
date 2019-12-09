bdd2612f95f1ca2d42db7e34ea68b4c414dd8eac
TheSharmaHimanshu <thesharmahimanshu@gmail.com>
2019-12-09 16:56:10 +0100

    Revised Car Categories Style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 
+/*Declare Tile
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
+} */
+
 export class AppComponent {
   title = 'GetWheelzProject';
+
+  /** Code for Category Tiles */
+ /* tiles: Tile[] = [
+    {text: 'One', cols: 3, rows: 1, color: 'lightblue'},
+    {text: 'Two', cols: 1, rows: 2, color: 'lightgreen'},
+    {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
+    {text: 'Four', cols: 2, rows: 1, color: '#DDBDF1'},
+  ]; */
 }
