8690d961ce472b4694561cadfb3be8cc599be6a2
gastonvejar <gaston.vejar@gmail.com>
2019-12-09 01:46:28 +0100

    ng-bootstrap for carousel in search results

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { MaterialModule } from './material/material.module';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/html/search_results.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './search_results.html',
+  styleUrls: ['../css/search_results.css']
+})
+export class AppComponent {
+  title = 'SearchResults';
+  img_car1 = "../assets/car_images/01_McLaren_F1.jpg";
+  img_car2 = "../assets/car_images/02_Ferrari_F50.jpg";
+  img_car3 = "../assets/car_images/03_Lamborghini_Murcielago.jpg";
+  img_car4 = "../assets/car_images/04_Maserati_GranTurismo.jpg";
+}
