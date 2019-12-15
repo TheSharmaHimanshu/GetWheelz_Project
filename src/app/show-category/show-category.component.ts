8f882e7db19501147fd19b444d6e1f40f9c4b6bf
gastonvejar <gaston.vejar@gmail.com>
2019-12-15 15:37:26 +0100

    dynamic categories

--- src/app/show-category/show-category.component.ts
+++ src/app/show-category/show-category.component.ts
@@ -10,16 +10,81 @@ import { DataService } from "../data.service";
 export class ShowCategoryComponent implements OnInit {
 
   page_title:string;
+  images_index:number;
+  category_cars;
 
   constructor(private data: DataService) { }
 
   ngOnInit() {
-    this.data.currentMessage.subscribe(message => this.page_title = message)
+    this.data.currentMessage.subscribe(message => this.getPageInfo(message))
   }
-  
-  img_car1 = "../assets/car_images/01_McLaren_F1.jpg";
-  img_car2 = "../assets/car_images/02_Ferrari_F50.jpg";
-  img_car3 = "../assets/car_images/03_Lamborghini_Murcielago.jpg";
-  img_car4 = "../assets/car_images/04_Maserati_GranTurismo.jpg";
 
+  getPageInfo(message){
+    var i:number;
+    this.page_title = message + ' Cars';
+    this.category_cars = this.cars.filter( car => car.category == message);
+  }
+
+  categories = [
+    'Sport',
+    'Performance',
+    'Luxury',
+    'Hatchback',
+    'SUV',
+    'Concept',
+    'Electric',
+    'GT',
+    'Convertible'
+  ];
+
+  cars = [
+    {
+      img: "../assets/car_images/01_McLaren_F1.jpg",
+      category: "Sport",
+      maker: "McLaren",
+      model: "F1",
+      body: "Coupé",
+      doors: 2,
+      speed: "387 km/h or 241 Mph",
+      engine: "V 12",
+      fuel: "Petrol",
+      proce: "$ 1,000,000"
+    },
+    {
+      img: "../assets/car_images/02_Ferrari_F50.jpg",
+      category: "Performance",
+      maker: "Ferrari",
+      model: "F50",
+      body: "Coupé",
+      doors: 2,
+      speed: "387 km/h or 241 Mph",
+      engine: "V 12",
+      fuel: "Petrol",
+      proce: "$ 1,000,000"
+    },
+    {
+      img: "../assets/car_images/03_Lamborghini_Murcielago.jpg",
+      category: "Performance",
+      maker: "Lamborghini",
+      model: "Murcielago",
+      body: "Coupé",
+      doors: 2,
+      speed: "387 km/h or 241 Mph",
+      engine: "V 12",
+      fuel: "Petrol",
+      proce: "$ 1,000,000"
+    },
+    {
+      img: "../assets/car_images/04_Maserati_GranTurismo.jpg",
+      category: "Sport",
+      maker: "Maserati",
+      model: "GranTurismo",
+      body: "Coupé",
+      doors: 2,
+      speed: "387 km/h or 241 Mph",
+      engine: "V 12",
+      fuel: "Petrol",
+      proce: "$ 1,000,000"
+    },
+  ];
 }
