c223a4fd4555283af1e84ff704f9cdc66d208d93
gastonvejar <gaston.vejar@gmail.com>
2019-12-18 12:13:26 +0100

    Change fav icon

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { MatButtonModule, MatMenuModule, MatToolbarModule, MatDialogModule, MatIconModule, MatInputModule, MatDividerModule, MatSlideToggleModule, MatGridListModule, MatExpansionModule, MatCardModule, MatBottomSheetModule, MatListModule, MatSelectModule, MatDatepickerModule, MatNativeDateModule } from '@angular/material';
+import { MatTooltipModule, MatButtonModule, MatMenuModule, MatToolbarModule, MatDialogModule, MatIconModule, MatInputModule, MatDividerModule, MatSlideToggleModule, MatGridListModule, MatExpansionModule, MatCardModule, MatBottomSheetModule, MatListModule, MatSelectModule, MatDatepickerModule, MatNativeDateModule } from '@angular/material';
 import { AdvanceSearchComponent } from '../advance-search/advance-search.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from '../login/login.component';
@@ -7,6 +7,7 @@ import { RegisterComponent } from '../register/register.component';
 
 
 const MaterialComponents = [
+  MatTooltipModule,
   MatButtonModule,
   MatToolbarModule,
   MatIconModule,
@@ -17,8 +18,8 @@ const MaterialComponents = [
 
 @NgModule({
   declarations: [AdvanceSearchComponent, LoginComponent, RegisterComponent],
-  imports: [MaterialComponents, MatMenuModule, MatNativeDateModule, MatDatepickerModule, MatDialogModule, MatSelectModule, BrowserAnimationsModule, MatListModule, MatBottomSheetModule, MatToolbarModule, MatIconModule, MatInputModule, MatDividerModule, MatSlideToggleModule, MatGridListModule, MatExpansionModule, MatCardModule],
-  exports: [MaterialComponents, MatMenuModule, MatNativeDateModule, MatDatepickerModule, MatDialogModule, MatSelectModule, MatListModule, BrowserAnimationsModule, MatBottomSheetModule, MatToolbarModule, MatIconModule, MatInputModule, MatDividerModule, MatSlideToggleModule, MatGridListModule, MatExpansionModule, MatCardModule],
+  imports: [MatTooltipModule, MaterialComponents, MatMenuModule, MatNativeDateModule, MatDatepickerModule, MatDialogModule, MatSelectModule, BrowserAnimationsModule, MatListModule, MatBottomSheetModule, MatToolbarModule, MatIconModule, MatInputModule, MatDividerModule, MatSlideToggleModule, MatGridListModule, MatExpansionModule, MatCardModule],
+  exports: [MatTooltipModule, MaterialComponents, MatMenuModule, MatNativeDateModule, MatDatepickerModule, MatDialogModule, MatSelectModule, MatListModule, BrowserAnimationsModule, MatBottomSheetModule, MatToolbarModule, MatIconModule, MatInputModule, MatDividerModule, MatSlideToggleModule, MatGridListModule, MatExpansionModule, MatCardModule],
   entryComponents: [AdvanceSearchComponent, LoginComponent, RegisterComponent]
 })
 export class MaterialModule { }
